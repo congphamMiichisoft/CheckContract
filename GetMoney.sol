f05be3ce70c647eb2991ca136ac0a255b91f4833
Pham Cong <congp@miichisoft.net>
2022-04-15 14:50:25 +0700

    using mdifier instead of "If"

--- GetMoney.sol
+++ GetMoney.sol
@@ -2,43 +2,40 @@ pragma solidity ^0.8.4;
 
 contract DemoMoney {
     address public minter;
-    mapping (address => uint) public balances;
+    mapping(address => uint256) public balances;
 
-    event Sent(address from, address to, uint amount);
+    event Sent(address from, address to, uint256 amount);
 
     constructor() {
         minter = msg.sender;
     }
 
-    function mint(address receiver, uint amount) public {
+    function mint(address receiver, uint256 amount) public {
         require(msg.sender == minter);
         balances[receiver] += amount;
     }
 
-    error InsufficientBalance(uint requested, uint available);
+    error InsufficientBalance(uint256 requested, uint256 available);
 
-    function send(address receiver, uint amount) public {
-        if (amount > balances[msg.sender])
-            revert InsufficientBalance({
-                requested: amount,
-                available: balances[msg.sender]
-            });
-
+    function send(address receiver, uint256 amount) public isValidBalance(msg.sender,amount) {
+        
         balances[msg.sender] -= amount;
         balances[receiver] += amount;
         emit Sent(msg.sender, receiver, amount);
     }
 
-    function getMoney(address from, address to, uint256 amount)public  {
-        if (amount > balances[from])
-            revert InsufficientBalance({
-                requested: amount,
-                available: balances[from]
-            });
-
+    function getMoney(
+        address from,
+        address to,
+        uint256 amount
+    ) public isValidBalance(from,amount){
         balances[from] -= amount;
         balances[to] += amount;
         emit Sent(from, to, amount);
+    }
 
+    modifier isValidBalance(address wallet, uint256 amount) {
+        require(amount < balances[wallet]);
+        _;
     }
 }
